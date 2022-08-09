9ceca419a2a327f264d1439a7e4ff110395efcfe
410nidhi <upasaninidhi@gmail.com>
2022-08-09 15:24:56 +0530

    Offline Localhost completed

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,12 +1,14 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 
-const connectDatabase =() => {
-    mongoose.connect('mongodb://localhost:27017/Ecommerce',
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-  }
-).then(data=>{console.log("Connected Database!")})
-}
+const connectDatabase = () => {
+  mongoose
+    .connect("mongodb://localhost:27017/Ecommerce", {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    .then((data) => {
+      console.log("Connected Database!");
+    });
+};
 
 module.exports = connectDatabase;
--- backend/server.js
+++ backend/server.js
@@ -1,40 +1,40 @@
-const app = require("./app")
-const mongoose=require('mongoose');
-const cloudinary = require("cloudinary")
+const app = require("./app");
+const mongoose = require("mongoose");
+const cloudinary = require("cloudinary");
 const connectDatabase = require("./config/database");
 
 // Handling Uncaught Execptions
-process.on("uncaughtException", err=>{
-    console.log(`Error: ${err.message}`)
-    console.log(`Shutting down the server due to Uncaught Execption`)
+process.on("uncaughtException", (err) => {
+  console.log(`Error: ${err.message}`);
+  console.log(`Shutting down the server due to Uncaught Execption`);
 
-    process.exit(1)
-})
+  process.exit(1);
+});
 
 // Connecting to Database
 connectDatabase();
 
-const CLOUD_NAME = "dr3lqouuy"
-const CLOUD_API_KEY = "725735348775534"
-const CLOUD_API_SECRET = "sjrsAxhwTPtHwP3nPQjAejJnDsg"
+const CLOUD_NAME = "dr3lqouuy";
+const CLOUD_API_KEY = "725735348775534";
+const CLOUD_API_SECRET = "sjrsAxhwTPtHwP3nPQjAejJnDsg";
 cloudinary.config({
-    cloud_name:CLOUD_NAME,
-    api_key: CLOUD_API_KEY,
-    api_secret:CLOUD_API_SECRET,
-    secure: true
-})
+  cloud_name: CLOUD_NAME,
+  api_key: CLOUD_API_KEY,
+  api_secret: CLOUD_API_SECRET,
+  secure: true,
+});
 
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 3000;
 const server = app.listen(port, () => {
-    console.log(`Server is working on http://localhost:${port}`)
-})
+  console.log(`Server is working on http://localhost:${port}`);
+});
 
 // Unhandled Promise Rejections
-process.on("unhandledRejection", err=>{
-    console.log(`Error: ${err.message}`)
-    console.log(`Shutting down the server due to Unhandled Promise Rejection`)
+process.on("unhandledRejection", (err) => {
+  console.log(`Error: ${err.message}`);
+  console.log(`Shutting down the server due to Unhandled Promise Rejection`);
 
-    server.close(()=>{
-        process.exit(1)
-    })
-})
+  server.close(() => {
+    process.exit(1);
+  });
+});
